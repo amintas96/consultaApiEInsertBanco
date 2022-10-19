42aa51199815b25cb3d8ab99d879d0e47d412cf2
Amintas <amintaspereira@hotmail.com>
2022-10-19 12:04:26 -0300

    incrimemento do schedule no codigo

--- AtualizaAcoesUsuarios.py
+++ AtualizaAcoesUsuarios.py
@@ -1,6 +1,9 @@
+from calendar import MONDAY
 import consultaApi
 import utils
 import logging
+import schedule
+import time
 
 logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s',filename='GerenciadorIdsElegiveis.log', level=logging.INFO)
 
@@ -9,3 +12,11 @@ def atualiza_usuarios_por_acao():
     consultaApi.verificaNaApiIDUsuarioPorAcao()
     utils.montaEComparaArquivo()
 
+
+
+schedule.every().monday.at('09:00').do(atualiza_usuarios_por_acao)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(300)
